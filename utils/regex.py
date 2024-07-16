02abcd899e961aa893690985892b39013e24d37a
YoursFunny <admin@yoursfunny.top>
2024-07-16 14:21:43 +0800

    update pixiv regex

--- utils/regex.py
+++ utils/regex.py
@@ -5,4 +5,4 @@ x_media_url = re.compile(r"^(?:https?://)(pbs|video)\.twimg\.com/(.*)")
 x_tco_url = re.compile(r"(?:https?://)t\.co/.+$", re.M)
 message_url = re.compile(r"\[.+]", re.S)
 
-pixiv_url = re.compile(r"^(?:https?://)?(?:www\.)?pixiv\.net/(?:en/)?artworks/(\d+)")
+pixiv_url = re.compile(r"^(?:https?://)?(?:www\.)?pixiv\.net/(?:en/)?(?:artworks/|i/)(\d+)")
